2c6b2b907e840e41022868dc1ce4236b9a49ac54
meghpatil21 <megh.after18@gmail.com>
2022-08-31 09:27:37 +0530

    [megh] Perfect number

---
+++ LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PerfectNumber
+    {
+        public static void PerfectNo()
+        {
+            int sum = 0;
+            int n;
+            Console.Write("Enter the Number: ");
+            int number =Convert.ToInt32(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,12 +6,14 @@
         {
             Console.WriteLine("Welcome to Logical Programs");
             Console.WriteLine();
-            Console.WriteLine("Choose an Option from following:\n 1.Fibonacci Series");
+            Console.WriteLine("Choose an Option from following:\n 1.Fibonacci Series\n 2.Perfect Number");
             int option=Convert.ToInt32(Console.ReadLine());
             
             switch (option)
             { 
                 case 1:FinonacciSeries.FibSeries();
+                    break;
+                case 2: PerfectNumber.PerfectNo();
                     break;
                 default:
                     Console.WriteLine("Invalid Choice");
