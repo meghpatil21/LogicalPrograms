04c4bfad622805f8519dcfef68b15b0195b40e15
meghpatil21 <megh.after18@gmail.com>
2022-08-31 08:51:49 +0530

    [megh] Fib Series

---
+++ LogicalPrograms/FinonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FinonacciSeries
+    {
+        public static void FibSeries()
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3, i, number;
+            Console.Write("Enter the number of fibonacci units: ");
+            number =Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2 + " ");    
+            for (i = 2; i < number; ++i)    
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine();
+            Console.WriteLine("Choose an Option from following:\n 1.Fibonacci Series");
+            int option=Convert.ToInt32(Console.ReadLine());
+            
+            switch (option)
+            { 
+                case 1:FinonacciSeries.FibSeries();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+        }
+    }
+}
