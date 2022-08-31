02f0704bd357c892146bcd960222fbbd1bbb5323
meghpatil21 <megh.after18@gmail.com>
2022-08-31 12:05:37 +0530

    [megh] Coupon Number

---
+++ LogicalPrograms/CouponNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class CouponNumbers
+    {
+        public static void CoupnNo()
+        {
+            Console.WriteLine("Enter how many coupon number: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int mem = 0;
+            int totalRandom = 0;
+            Console.WriteLine();
+            while(n!=count)
+            {
+                Random random = new Random();
+                int CoupnNo=random.Next(1,9);
+                totalRandom++;
+
+                if(CoupnNo!=mem)
+                {
+                    Console.WriteLine(CoupnNo);
+                    mem = CoupnNo;
+                    count++;
+                }
+
+            }
+            Console.WriteLine($"Total Random number needed to generate non-repeated {n} coupons number is {totalRandom}");
+            
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Logical Programs");
             Console.WriteLine();
-            Console.WriteLine("Choose an Option from following:\n 1.Fibonacci Series\n 2.Perfect Number\n 3.Prime Number\n 4.Reverse Number");
+            Console.WriteLine("Choose an Option from following:\n 1.Fibonacci Series\n 2.Perfect Number\n 3.Prime Number\n 4.Reverse Number\n 5.Coupon number");
             int option=Convert.ToInt32(Console.ReadLine());
             
             switch (option)
@@ -18,6 +18,8 @@
                 case 3: PrimeNumber.PrimeNo();
                     break;
                 case 4: ReverseNumber.RevNo();
+                    break;
+                case 5: CouponNumbers.CoupnNo();
                     break;
                 default:
                     Console.WriteLine("Invalid Choice");
